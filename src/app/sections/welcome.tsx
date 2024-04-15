0242b0737ba3d5983bedce3bb1473c149c423f7c
Fernando Tamayo <ftamayo1998@gmail.com>
2024-04-16 00:44:56 +0100

    welcome image

--- src/app/sections/welcome.tsx
+++ src/app/sections/welcome.tsx
@@ -1,6 +1,8 @@
 "use client";
 import "../styles/welcome.css";
 import { TbArrowBadgeDown } from "react-icons/tb";
+import Image from "next/image";
+import Me from "/public/me.jpg";
 
 export default function WelcomeSection() {
   const downloadPdf = () => {
@@ -17,38 +19,52 @@ export default function WelcomeSection() {
   };
 
   return (
-    <section className="flex flex-col justify-center gap-[48px]  h-screen">
-      <div className="flex flex-col gap-[16px]">
-        <div className="text-xl">
-          Hello there, I'm <span className="nameMain font-bold">Fernando</span>{" "}
-        </div>
-        <div className="font-medio xl:text-8xl md:text-7xl text-5xl flex flex-col w-[280px] md:w-[550px] xl:w-[720px]">
-          <div className=" partialUnder text-cyan-500 relative ">
-            Full-Stack
+    <section className="h-screen">
+      <div className="absolute scale-[80%] top-[5%] left-[15%] sm:left-auto sm:right-0  ">
+        <div className=" h-full w-full  bg-gradient-radial from-transparent via-backgroundLight via-[70%] to-backgroundLight scale-105  z-20 absolute"></div>
+        <Image
+          src={Me}
+          alt={"A nice photo of me"}
+          priority
+          className="opacity-1"
+        />
+      </div>
+      <div className="flex flex-col justify-center gap-[48px] h-full relative pt-48">
+        <div className="flex flex-col gap-[16px]">
+          <div className="text-xl">
+            Hello there, I'm{" "}
+            <span className="nameMain font-bold">Fernando</span>{" "}
           </div>
-          <div className=" text-right">Developer</div>
+          <div className="font-medio xl:text-8xl md:text-7xl text-5xl flex flex-col w-[280px] md:w-[550px] xl:w-[720px]">
+            <div className=" partialUnder text-cyan-500 relative ">
+              Full-Stack
+            </div>
+
+            <div className=" text-right">Developer</div>
+          </div>
+          <div className="text-xs font-extralight">
+            Lorem ipsum dolor sit amet, veritus euripidis sed ne, nam in illud
+            fabulas. Id partem altera animal sit, platonem facilisis est in, ut
+            vel eirmod epicuri.
+          </div>
         </div>
-        <div className="text-xs font-extralight">
-          Lorem ipsum dolor sit amet, veritus euripidis sed ne, nam in illud
-          fabulas. Id partem altera animal sit, platonem facilisis est in, ut
-          vel eirmod epicuri.
+
+        <div className="flex sm:flex-row flex-col gap-2 sm:gap-6">
+          <button
+            onClick={downloadPdf}
+            className="text-sm hover:shadow-lg hover:shadow-cyan-300/50 transition-shadow duration-500 rounded-3xl p-4 sm:p-4 bg-cyan-300 sm:w-38 text-white">
+            Download CV
+          </button>
+          <button className="text-sm text-cyan-300 rounded-3xl border-4 hover:bg-gradient-to-r from-red-700 to-yellow-600 border-cyan-300 p-4 w-38 hover:shadow-inner">
+            Get in touch
+          </button>
         </div>
-      </div>
-      <div className="flex sm:flex-row flex-col gap-2 sm:gap-6">
-        <button
-          onClick={downloadPdf}
-          className="text-sm hover:shadow-lg hover:shadow-cyan-300/50 transition-shadow duration-500 rounded-3xl p-4 sm:p-4 bg-cyan-300 sm:w-38 text-white">
-          Download CV
-        </button>
-        <button className="text-sm text-cyan-300 rounded-3xl border-4 hover:bg-gradient-to-r from-red-700 to-yellow-600 border-cyan-300 p-4 w-38 hover:shadow-inner">
-          Get in touch
-        </button>
-      </div>
-      <div className="flex flex-row justify-center w-full">
-        <div className="flex flex-col justify-between">
-          <TbArrowBadgeDown className="scrollArrow animate-pulse" />
-          <TbArrowBadgeDown className="scrollArrow " />
-          <TbArrowBadgeDown className="scrollArrow " />
+        <div className="flex flex-row justify-center w-full">
+          <div className="flex flex-col justify-between">
+            <TbArrowBadgeDown className="scrollArrow animate-pulse" />
+            <TbArrowBadgeDown className="scrollArrow " />
+            <TbArrowBadgeDown className="scrollArrow " />
+          </div>
         </div>
       </div>
     </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        backgroundLight: {
+          DEFAULT: "rgb(243, 249, 248)",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":

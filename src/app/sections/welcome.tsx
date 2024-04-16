7081f9cf8cd703303b411a3179caf405ca5c193c
Fernando Tamayo <ftamayo1998@gmail.com>
2024-04-16 20:13:29 +0100

    images and gradient

--- src/app/sections/welcome.tsx
+++ src/app/sections/welcome.tsx
@@ -20,14 +20,17 @@ export default function WelcomeSection() {
 
   return (
     <section className="h-screen">
-      <div className="absolute  top-[2%] left-[15%] sm:left-auto sm:right-0">
-        <div className="h-full w-full bg-gradient-radial from-transparent via-backgroundLight  via-[60%] to-backgroundLight scale-105  absolute"></div>
-        <Image
-          src={Me}
-          alt={"A nice photo of me"}
-          priority
-          className="opacity-1 rounded-full"
-        />
+      <div className="absolute  top-[5%] left-[15%] sm:left-auto sm:right-0">
+        <div>
+          <div className="h-full w-full  bg-gradient-to-bl from-transparent via-backgroundLight  via-[70%] to-backgroundLight scale-105  absolute"></div>
+          <Image
+            src={Me}
+            alt={"A nice photo of me"}
+            priority
+            className="opacity-1 rounded-full"
+          />
+        </div>
+        <div className=" w-24 h-24 border-4 rounded-full bg-cyan-300 absolute top-[60%] right-[-10%] sm:right-[12%] sm:top-[80%] z-0 sm:h-32 sm:w-32 sm:border-8 border-backgroundLight" />
       </div>
       <div className=" flex flex-col justify-center gap-[16px] h-full relative pt-[60%] sm:pt-[0%] xl:pt-[0%]">
         <div className="flex flex-col gap-[16px]">
@@ -53,7 +56,7 @@ export default function WelcomeSection() {
             className="text-sm hover:shadow-lg hover:shadow-cyan-300/50 transition-shadow duration-500 rounded-3xl p-4 sm:p-4 bg-cyan-300 sm:w-38 text-white">
             Download CV
           </button>
-          <button className="text-sm text-cyan-300 rounded-3xl border-4 hover:bg-gradient-to-r from-red-700 to-yellow-600 border-cyan-300 p-4 w-38 hover:shadow-inner">
+          <button className="  text-sm text-cyan-300 rounded-3xl border-4 hover:bg-gradient-to-r from-red-700 to-yellow-600 border-cyan-300 p-4 w-38 hover:shadow-inner">
             Get in touch
           </button>
         </div>

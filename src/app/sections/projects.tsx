9dfd2e77e9476e585dc41776fc8d4a61c4b26b67
Fernando Tamayo <ftamayo1998@gmail.com>
2024-04-27 23:47:47 +0100

    test

--- src/app/sections/projects.tsx
+++ src/app/sections/projects.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import projectsObj from "../../assets/json/projects.json";
 import { FaChevronRight } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
+import "../styles/projects.css";
+import { useSwipeable } from "react-swipeable";
 
 interface IProject {
   name: string;
@@ -18,27 +20,19 @@ export default function ProjectsSection({
   forwardedRef: LegacyRef<HTMLElement> | undefined;
 }) {
   const [position, setPosition] = useState(0);
-
+  console.log(position);
   const setNext = () => {
-    clearInterval(1000);
-    setPosition(
+    setPosition((position) =>
       position === projectsObj.projects.length - 1 ? 0 : position + 1
     );
   };
 
   const setPrev = () => {
-    clearInterval(1000);
+    // clearInterval(1000);
     setPosition(
       position === 0 ? projectsObj.projects.length - 1 : position - 1
     );
   };
-  console.log(position);
-
-  useEffect(() => {
-    setInterval(setNext, 1000);
-
-    return () => clearInterval(1000);
-  }, []);
 
   function ProjectCard({
     project,
@@ -49,10 +43,11 @@ export default function ProjectsSection({
   }) {
     return (
       <div
-        className={`w-[100%] flex-shrink-0 shadow-md rounded-md overflow-hidden transition-all duration-500 ${
-          position !== index
-            ? "transform scale-75 opacity-65"
-            : "transform scale-100 opacity-100"
+        onClick={() => {
+          setPosition(index);
+        }}
+        className={`imageCard w-[100%]  flex-shrink-0 shadow-md rounded-md overflow-hidden ${
+          position === index && "selected"
         }`}>
         <Image src={tempImage} alt="temp" className="" />
         <div className="px-4 py-2">
@@ -64,8 +59,15 @@ export default function ProjectsSection({
     );
   }
 
+  const handlers = useSwipeable({
+    onSwipedLeft: (eventData) => setNext(),
+    onSwipedRight: (eventData) => setPrev(),
+  });
+  console.log(`-translate-x-[${100 * position}%]`);
+
   return (
     <section
+      {...handlers}
       ref={forwardedRef}
       className="  flex flex-col items-center gap-4  h-full py-4">
       <div className="text-2xl font-semibold text-cyan-500 w-full">
@@ -79,7 +81,8 @@ export default function ProjectsSection({
 
         <div className=" w-[60%] ">
           <div
-            className={`flex  -translate-x-[${
+            {...handlers}
+            className={`flex -translate-x-[${
               100 * position
             }%] transition-all duration-500`}>
             {projectsObj.projects.map((project, index) => {

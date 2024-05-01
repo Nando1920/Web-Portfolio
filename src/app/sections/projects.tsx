7b5040e05578c2935c05ed7828580c605623f6a6
Fernando Tamayo <ftamayo1998@gmail.com>
2024-05-02 00:14:22 +0100

    improvements to projects section for other screens

--- src/app/sections/projects.tsx
+++ src/app/sections/projects.tsx
@@ -2,16 +2,19 @@
 import { LegacyRef, useEffect, useState } from "react";
 import Image from "next/image";
 import projectsObj from "../../assets/json/projects.json";
-import { FaChevronRight } from "react-icons/fa";
+import { FaChevronRight, FaCode } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
 import "../styles/projects.css";
 import { useSwipeable } from "react-swipeable";
 import { getProjectImg } from "../utils/utils";
+import { CgDetailsMore } from "react-icons/cg";
+import Link from "next/link";
 
 interface IProject {
   name: string;
   client: string;
   text: string;
+  link: string;
 }
 
 export default function ProjectsSection({
@@ -39,34 +42,54 @@ export default function ProjectsSection({
   function ProjectCard({
     project,
     index,
+    transition,
   }: {
     project: IProject;
     index: number;
+    transition: boolean;
   }) {
+    if (!transition) {
+      setAuto(false);
+    }
     return (
       <div
         onClick={() => {
-          setPosition(index);
-          setAuto(false);
+          if (transition) {
+            setPosition(index);
+            setAuto(false);
+          } else {
+          }
         }}
-        className={`imageCard w-[100%]  flex-shrink-0 shadow-md rounded-lg overflow-hidden ${
-          position === index && "selected"
-        }`}>
+        className={`${
+          transition
+            ? "imageCard w-[100%]  flex-shrink-0 shadow-md rounded-lg overflow-hidden"
+            : "w-[100%] overflow-hidden scale-100 md:min-w-[200px] lg:min-w-[250px] xl:min-w-[350px] "
+        } ${position === index && transition && "selected sm:selected"} `}>
         <Image
           loading="lazy"
           src={getProjectImg(project.name)}
           alt="temp"
           placeholder="blur"
-          className="absolute z-[-1] bg-cover"
+          className="absolute z-[-1] bg-cover "
         />
-        <div className="flex flex-col gap-1 p-4 bg-backgroundLight mt-36 rounded-t-xl h-full">
-          <div className=" font-bold text-xl">{project.name}</div>
-          <div className=" text-primary">{project.client}</div>
+        <div className=" hidden opacity-0 md:flex justify-center items-center gap-10  absolute z-[1] bg-cover transition-color duration-300  hover:bg-black hover:opacity-60  w-full h-full">
+          <Link
+            href={`/projects?project=${project.name}`}
+            className="rounded-full border-2  p-2">
+            <CgDetailsMore className="w-10 h-10  text-white" />
+          </Link>
+          <Link href={project.link} className="rounded-full border-2 p-2">
+            <FaCode className="w-10 h-10  text-white" />
+          </Link>
+        </div>
+        <div className="flex flex-col gap-1 p-4 bg-backgroundLight mt-36 rounded-t-xl h-full  ">
+          <div className=" font-bold text-xl md:text-xl">{project.name}</div>
+          <div className=" text-primary md:text-lg">{project.client}</div>
 
-          <div className="line-clamp-[8]">{project.text}</div>
+          <div className="line-clamp-[8] md:hidden">{project.text}</div>
           <a
             href={`/projects?project=${project.name}`}
-            className="text-primaryBold underline">
+            className="text-primaryBold underline md:hidden">
             Find out more
           </a>
         </div>
@@ -94,16 +117,32 @@ export default function ProjectsSection({
       <div className="shapedividers_com-6562 h-12 w-screen"></div>
 
       <div className=" top-0 transform w-screen h-full absolute z-[-1] bg-primary" />
-      <div className="text-2xl font-semibold text-white w-full">Projects</div>
-      <div className="flex items-center justify-center gap-4">
+      <div className="text-2xl sm:text-4xl font-semibold text-white w-full">
+        Projects
+      </div>
+
+      <div className="md:grid md:grid-cols-3 gap-24 hidden h-fit  ">
+        {projectsObj.projects.map((project, index) => {
+          return (
+            <ProjectCard
+              key={project.name}
+              project={project}
+              index={index}
+              transition={false}
+            />
+          );
+        })}
+      </div>
+
+      <div className="md:hidden flex items-center justify-center gap-4">
         <FaChevronLeft
           onClick={() => {
             setPrev(true);
           }}
-          className="shadow p-1 w-6 h-6  z-20 bg-backgroundLight text-primary rounded-full"
+          className=" shadow p-1 w-6 h-6  z-20 bg-backgroundLight text-primary rounded-full"
         />
 
-        <div className=" w-[60%] ">
+        <div className=" w-[60%]">
           <div
             {...handlers}
             className="flex transition-transform  duration-500"
@@ -114,13 +153,14 @@ export default function ProjectsSection({
                   key={project.name}
                   project={project}
                   index={index}
+                  transition
                 />
               );
             })}
           </div>
         </div>
         <FaChevronRight
-          className="shadow p-1 w-6 h-6 z-20  bg-backgroundLight text-primary rounded-full"
+          className=" shadow p-1 w-6 h-6 z-20  bg-backgroundLight text-primary rounded-full"
           onClick={() => {
             setNext(true);
           }}

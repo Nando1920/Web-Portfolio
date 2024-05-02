4e0d1e3af6c628e48244e9277177dd3ffcc0a6d0
Fernando Tamayo <ftamayo1998@gmail.com>
2024-05-03 00:39:53 +0100

    some fun with motion

--- src/app/sections/welcome.tsx
+++ src/app/sections/welcome.tsx
@@ -3,23 +3,23 @@ import "../styles/welcome.css";
 import Image from "next/image";
 import Me from "/public/me.jpg";
 import { TbArrowBadgeDown } from "react-icons/tb";
+import { downloadPdf } from "../utils/utils";
+import { motion } from "framer-motion";
 
 export default function WelcomeSection({ onClick }: { onClick: Function }) {
-  const downloadPdf = () => {
-    const pdfUrl = "/Fernando_TamayoCV2024.pdf";
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.download = pdfUrl.split("/").pop() ?? "My_CV";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   return (
     <section className=" shadow-2xl h-[100svh]  flex justify-around items-center sm:items-start flex-col rounded-b-[40px] sm:rounded-b-[72px]  bg-backgroundLight relative">
       <div className="absolute w-screen h-full z-[-1] bg-gradient-to-r from-secondaryBold to-secondary bottom-0" />
       <div className="flex justify-start px-4 sm:px-12 xl:px-24 2xl:px-52 ">
-        <div className="absolute  top-[5%] sm:top-[15%] left-[15%] sm:left-auto sm:right-10 overflow-clip sm:overflow-visible sm:scale-110 2xl:scale-125 2xl:top-[20%] 2xl:right-[10%]">
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, x: 75 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.5 }}
+          className="absolute  top-[5%] sm:top-[15%] left-[15%] sm:left-auto sm:right-10 overflow-clip sm:overflow-visible sm:scale-110 2xl:scale-125 2xl:top-[20%] 2xl:right-[10%]">
           <div className="h-full w-full  bg-gradient-to-bl from-transparent via-backgroundLight  via-[70%] to-backgroundLight  absolute"></div>
           <Image
             src={Me}
@@ -28,8 +28,16 @@ export default function WelcomeSection({ onClick }: { onClick: Function }) {
             className="opacity-1 rounded-full"
           />
           <div className=" h-24 w-24  border-4 rounded-full bg-gradient-to-tr from-cyan-400 via-secondary via-95%  to-primaryBold absolute top-[60%] right-[-10%] sm:right-[12%] sm:top-[80%] z-0 sm:h-40 sm:w-40 sm:border-8 border-backgroundLight" />
-        </div>
-        <div className="flex flex-col justify-center gap-[24px] h-full relative pt-[60%] sm:pt-[20%]  ">
+        </motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, x: -75 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.5 }}
+          className="flex flex-col justify-center gap-[24px] h-full relative pt-[60%] sm:pt-[20%]  ">
           <div className="flex flex-col gap-[16px] 2xl:gap-[32px]">
             <div className="text-xl sm:text-3xl 2xl:text-4xl">
               Hello there, I'm{" "}
@@ -61,15 +69,23 @@ export default function WelcomeSection({ onClick }: { onClick: Function }) {
               Get in touch
             </button>
           </div>
-        </div>
+        </motion.div>
       </div>
-      <div className="flex flex-row justify-center w-full">
+      <motion.div
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5 }}
+        className="flex flex-row justify-center w-full">
         <div className="flex flex-col justify-between">
           <TbArrowBadgeDown className="scrollArrow" />
           <TbArrowBadgeDown className="scrollArrow" />
           <TbArrowBadgeDown className="scrollArrow" />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -12,3 +12,13 @@ export const getProjectImg = (name: string) => {
       return tempImage;
   }
 };
+
+export const downloadPdf = () => {
+  const pdfUrl = "/Fernando_TamayoCV2024.pdf";
+  const link = document.createElement("a");
+  link.href = pdfUrl;
+  link.download = pdfUrl.split("/").pop() ?? "My_CV";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
